5e741f7457d18e86c5703d33a61fd80ec831be2c
vis4rd <alagaesia.vrael@gmail.com>
2022-12-16 21:16:31 +0100

    🎨 Simplify structure of the site

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,46 @@
+<script setup>
+    import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <header>
+        <div class="greetings">
+            <h1>You did it</h1>
+            <h3>I am somewhat of a scientist myself.</h3>
+        </div>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    h1,
+    h3 {
+        font-weight: normal;
+        text-align: center;
+    }
+
+    nav {
+        width: 100%;
+        text-align: center;
+    }
+
+    nav a {
+        display: inline-block;
+        padding: 0 1rem;
+        border-left: 1px solid var(--color-border);
+    }
+
+    nav a:first-of-type {
+        border: 0;
+    }
+
+    header {
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+        /* padding-right: 80px; */
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
 <script setup>
-    import TheWelcome from "../components/TheWelcome.vue";
+    import WelcomeItem from "../components/WelcomeItem.vue";
 </script>
 
 <template>
-    <main>
-        <TheWelcome />
-    </main>
+    <WelcomeItem>
+        <template #heading>Documentation</template>
+
+        Vue's official documentation provides you with all information you need to get started.
+    </WelcomeItem>
+
+    <WelcomeItem>
+        <template #heading>Tooling</template>
+
+        This project is served and bundled with Vite. The recommended IDE setup is VSCode + Volar.
+        If you need to test your components and web pages, check out Cypress and Cypress Component
+        Testing.
+
+        <br />
+
+        More instructions are available in <code>README.md</code>.
+    </WelcomeItem>
+
+    <WelcomeItem>
+        <template #heading>Ecosystem</template>
+
+        Get official tools and libraries for your project: Pinia, Vue Router, Vue Test Utils, and
+        Vue Dev Tools. If you need more resources, we suggest paying Awesome Vue a visit.
+    </WelcomeItem>
+
+    <WelcomeItem>
+        <template #heading>Community</template>
+
+        Got stuck? Ask your question on Vue Land, our official Discord server, or StackOverflow. You
+        should also subscribe to our mailing list and follow the official @vuejs twitter account for
+        latest news in the Vue world.
+    </WelcomeItem>
+
+    <WelcomeItem>
+        <template #heading>Support Vue</template>
+
+        As an independent project, Vue relies on community backing for its sustainability. You can
+        help us by becoming a sponsor.
+    </WelcomeItem>
 </template>
