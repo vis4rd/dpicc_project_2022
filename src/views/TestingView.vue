09605e4c74e49e3a42065c76990980f4f4f0acf1
vis4rd <alagaesia.vrael@gmail.com>
2022-12-17 23:53:12 +0100

    🎨 Use OptionsGroup in a TestingView instead of manual v-fors

--- src/views/TestingView.vue
+++ src/views/TestingView.vue
@@ -1,39 +1,48 @@
 <script setup>
-    import CheckboxButton from "../components/CheckboxButton.vue";
+    // import CheckboxButton from "../components/CheckboxButton.vue";
+    import OptionsGroup from "../components/OptionsGroup.vue";
     import { TempoStore } from "../stores/tempo.js";
+    import { EnvironmentStore } from "../stores/environment.js";
+    import { FullnessStore } from "../stores/fullness.js";
+    import { MoodStore } from "../stores/mood.js";
+    import { TypeStore } from "../stores/type.js";
 
     const tempoStore = TempoStore();
+    const environmentStore = EnvironmentStore();
+    const fullnessStore = FullnessStore();
+    const moodStore = MoodStore();
+    const typeStore = TypeStore();
 </script>
 
 <template>
     <h2>This is an about page</h2>
-    options = {{ tempoStore.options }}
-    <br />
 
-    <div class="options_group">
-        <template
-            v-for="option in tempoStore.options"
-            :key="option"
-        >
-            <CheckboxButton
-                :on_click_func="tempoStore.updateSelected"
-                :label="option"
-                :checked="tempoStore.isSelected(option)"
-            />
-        </template>
-    </div>
+    <OptionsGroup :targetStore="tempoStore">
+        <h3 class="option_group_h3">Choose your favourite music tempo:</h3>
+    </OptionsGroup>
 
-    <br />
-    selected = {{ tempoStore.selected }}
-    <br />
-    unselected = {{ tempoStore.unselected }}
+    <OptionsGroup :targetStore="environmentStore">
+        <h3 class="option_group_h3">What environment do you listen to your music in?</h3>
+    </OptionsGroup>
+
+    <OptionsGroup :targetStore="fullnessStore">
+        <h3 class="option_group_h3">How full of noises and sounds is the music you listen to?</h3>
+    </OptionsGroup>
+
+    <OptionsGroup :targetStore="moodStore">
+        <h3 class="option_group_h3">Specify what mood of music you enjoy the most:</h3>
+    </OptionsGroup>
+
+    <OptionsGroup :targetStore="typeStore">
+        <h3 class="option_group_h3">
+            Do you have a preference between instrumental or vocal music?
+        </h3>
+    </OptionsGroup>
 </template>
 
 <style scoped>
-    .options_group {
-        display: flex;
-        width: auto;
-
-        border: solid white 1px;
+    .option_group_h3 {
+        margin: 0;
+        padding: 0;
     }
 </style>
